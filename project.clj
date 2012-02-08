113081df240461361435a982368592bd0780183f
Matthias Diehn Ingesman <matthias@ingesman.dk>
2012-02-08 18:22:09 +0100

    Updated to clojure 1.3 and converted to protocols

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject clj-htmlunit "0.0.1-SNAPSHOT"
-  :description "Clojure simple wrapper for htmlunit"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [net.sourceforge.htmlunit/htmlunit "2.8"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+(defproject clj-htmlunit "0.1.0"
+  :description "Simple clojure wrapper for htmlunit"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [net.sourceforge.htmlunit/htmlunit "2.9"]])
--- src/clj_htmlunit/core.clj
+++ src/clj_htmlunit/core.clj
@@ -1,42 +1,72 @@
 (ns clj-htmlunit.core
-  (:import (com.gargoylesoftware.htmlunit WebClient BrowserVersion))
-  (:use [clojure.string :only [upper-case]]))
-
-(def *client*)
+  (:import (com.gargoylesoftware.htmlunit
+            SgmlPage WebClient BrowserVersion)
+           (com.gargoylesoftware.htmlunit.html
+            HtmlElement)))
 
 (defn make-client
+  "[]
+    Create a WebClient acting as Internet Explorer 7.
+
+  [version]
+   Create a WebClient acting as one version, which can
+   be one of [:IE6 :IE7 :IE8 :FF3 FF3.6]."
+
   ([]
-     (make-client (.getNickname (BrowserVersion/getDefault))))
+     (make-client :IE7))
   ([version]
-     (let [v (upper-case version)]
-       (new WebClient
-	    (cond
-	     (= v "IE7") BrowserVersion/INTERNET_EXPLORER_7
-	     (= v "IE6") BrowserVersion/INTERNET_EXPLORER_6
-	     (= v "IE8") BrowserVersion/INTERNET_EXPLORER_8
-	     (= v "FF3") BrowserVersion/FIREFOX_3
-	     (= v "FF3.6") BrowserVersion/FIREFOX_3_6
-	     :else (throw (Exception. (str "Browser " version " is unknown"))))))))
+     (new WebClient
+          (case version
+            :IE7 BrowserVersion/INTERNET_EXPLORER_7
+            :IE6 BrowserVersion/INTERNET_EXPLORER_6
+            :IE8 BrowserVersion/INTERNET_EXPLORER_8
+            :FF3 BrowserVersion/FIREFOX_3
+            :FF3.6 BrowserVersion/FIREFOX_3_6
+            (throw (Exception. (str "Browser " (name version) " is unknown")))))))
 
 (defn get-page
+  "Use client to access url represented either as a
+  string or an URL object. Returns a page which can be
+  an HTML/XHTML/XML/SGML/Binary/Text/Unexpected page
+  depending on the content type of the response."
   [client url]
   (.getPage client url))
 
-(defn get-nodes-by-xpath
-  [node xpath]
-  (.getByXPath node xpath))
+(defprotocol XPathable
+  "A protocol for pages that are queryable using XPath."
 
-(defn get-first-node-by-xpath
-  [node xpath]
-  (.getFirstByXPath node xpath))
+  (descendants-by-xpath [node xpath]
+    "Returns all elements rooted at node matching xpath.")
 
-(defn get-node-anchors
-  [node]
-  (get-nodes-by-xpath node "//a"))
+  (first-descendant-by-xpath [node xpath]
+    "Returns the first element rooted at node matchin
+  xpath.")
 
-(defn get-nodes-anchors
-  [nodes]
-  (flatten (map #(get-node-anchors %) nodes))) 
+  (id [node]
+    "ID"))
+
+(extend-protocol XPathable
+  SgmlPage
+
+  (descendants-by-xpath [node xpath]
+    (.getByXPath node xpath))
+
+  (first-descendant-by-xpath [node xpath]
+    (.getFirstByXPath node xpath))
+
+  (id [node] node))
+
+(defprotocol Clickable
+  "A protocol for clickable DOM elements."
+
+  (click [node]
+    "Send a click event to node. Returns a complete page."))
+
+(extend-protocol Clickable
+  HtmlElement
+
+  (click [element]
+    (.click element)))
 
 (defn get-node-attributes
   [node]
